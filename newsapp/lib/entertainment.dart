f215c0bf2b83a10f68232e263e93597614357c79
Aman Ranabhat <080bct013.aman@pcampus.edu.np>
2026-01-12 17:57:48 +0545

    Update entertainment.dart

--- newsapp/lib/entertainment.dart
+++ newsapp/lib/entertainment.dart
@@ -44,6 +44,7 @@ class _EntertainmentwidgetState extends State<Entertainmentwidget> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
+        backgroundColor: Colors.blueGrey,
         title: const Text(
           'News app',
           style: TextStyle(fontSize: 24, fontWeight: FontWeight.bold),
