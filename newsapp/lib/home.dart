4ecbe564951f3351f633ab2eb4c993a3c57d99f0
Aman Ranabhat <080bct013.aman@pcampus.edu.np>
2026-01-12 17:57:30 +0545

    Update home.dart

--- newsapp/lib/home.dart
+++ newsapp/lib/home.dart
@@ -44,6 +44,7 @@ class _HomepageState extends State<Homepage> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
+        backgroundColor: Colors.blueGrey,
         title: const Text(
           'News app',
           style: TextStyle(fontSize: 24, fontWeight: FontWeight.bold),
